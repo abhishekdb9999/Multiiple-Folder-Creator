fa6d98227dd91da3cf32b2965d319d51190df002
ABHISHEK DEVARAJ  BHANGI <113203839+abhishekdb9999@users.noreply.github.com>
2024-01-20 11:00:00 +0530

    Create folder_creator.py

---
+++ folder_creator.py
@@ -0,0 +1,11 @@
+import os
+
+folder = []
+n = int(input("How many folders do you want???"))
+
+for folnames in range(n):
+    folder_name = input("Please Enter your folder name: ")
+    folder.append(folder_name)
+
+for folder_name in folder:
+    os.makedirs(folder_name)
